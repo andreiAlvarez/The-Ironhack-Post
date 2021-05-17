c99084c99dbd5fa24b36de2b9cdb05a19c1401da
Andrei Alvarez <andreialvarezf@gmail.com>
2021-05-17 11:43:57 -0500

    seeds data update

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,7 @@
 import { Headline } from "./Headline";
 
-
 function News(props) {
-  const { data } = require("../data.json");
+  const data = require("../data.json");
   console.log(data);
 
   return (
@@ -10,8 +9,11 @@ function News(props) {
       {/* <CategoriesBar /> */}
       <div className="all__news">
         {data
-          ? data.articles.map((news) =>
-          news.source.name !== "Google News" && <Headline data={news} key={news.url} /> 
+          ? data.map(
+              (news) =>
+                news.source.name !== "Google News" && (
+                  <Headline data={news} key={news.url} />
+                )
             )
           : "Loading"}
         <hr className="cover-lines"></hr>
